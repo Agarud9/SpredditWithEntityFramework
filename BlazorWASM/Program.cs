a3fe72bba40b85f54f2f1a6b066c18e807c66120
Enbi81 <wotng2002@gmail.com>
2022-11-01 11:16:56 +0100

    fixing problems

--- BlazorWASM/Program.cs
+++ BlazorWASM/Program.cs
@@ -5,6 +5,7 @@ using HttpClients.ClientInterfaces;
 using HttpClients.Implementations;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
